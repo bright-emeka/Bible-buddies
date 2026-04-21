369f4743b8adbb78ca6d1297b64ece6ebd17345b
bright-emeka <emeka0180@gmail.com>
2026-04-21 14:47:52 +0100

    finally

--- frontend/src/services/firebase.jsx
+++ frontend/src/services/firebase.jsx
@@ -1,20 +1,26 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCj7vAwDZKkqlf1e9kLko4ztol6ofxvW0c",
+  apiKey: "AIzaSyCj7vAwDZKkqlf1e9kLko4ztol6ofxvwOc",
   authDomain: "faith-social-ef895.firebaseapp.com",
+  databaseURL: "https://faith-social-ef895-default-rtdb.firebaseio.com",
   projectId: "faith-social-ef895",
   storageBucket: "faith-social-ef895.firebasestorage.app",
   messagingSenderId: "485831767405",
-  appId: "1:485831767405:web:9fa4e7ad111cde6951d664"
+  appId: "1:485831767405:web:c858ecef63de122f51d664",
+  measurementId: "G-C5M1353WGT"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Export for use in your Signup/Login components
+
+export const analytics = getAnalytics(app);
+
+// Export for use in Signup/Login components
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export default app;
